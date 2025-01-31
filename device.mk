d9b20797fa67ae5ad64f51df1831a7388abeb148
EmanuelCN <emanuelghub@gmail.com>
2025-01-31 19:49:55 +0000

    nabu: Apply Android Go optimizations
    Signed-off-by: kvsnr113 <kvsnrprojkt113@gmail.com>

--- device.mk
+++ device.mk
@@ -207,6 +207,18 @@ PRODUCT_PACKAGES += \
 # Dex
 PRODUCT_ART_TARGET_INCLUDE_DEBUG_BUILD := false
 USE_DEX2OAT_DEBUG := false
+# Optimize everything for preopt
+PRODUCT_DEX_PREOPT_DEFAULT_COMPILER_FILTER := everything
+# Always preopt extracted APKs to prevent extracting out of the APK for gms modules
+PRODUCT_ALWAYS_PREOPT_EXTRACTED_APK := true
+# Speed profile services and wifi-service to reduce RAM and storage
+PRODUCT_SYSTEM_SERVER_COMPILER_FILTER := speed-profile
+# Use a profile based boot image for this device
+PRODUCT_USE_PROFILE_FOR_BOOT_IMAGE := true
+PRODUCT_DEX_PREOPT_BOOT_IMAGE_PROFILE_LOCATION := frameworks/base/boot/boot-image-profile.txt
+
+PRODUCT_DEXPREOPT_SPEED_APPS += \
+    Settings
 
 # Display
 PRODUCT_PACKAGES += \
