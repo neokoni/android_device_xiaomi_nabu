24d97040a365a2d2147289bc168490500d1de092
neokoni <dulin081682@126.com>
2026-05-01 23:57:26 +0800

    Revert "nabu: allow tinymix and friends to be bundled in regardless of build-type"
    This reverts commit d2cbf21f2e04c2f0219e333868a158f84b311fd1.

--- device.mk
+++ device.mk
@@ -140,8 +140,8 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     audio_amplifier.qcom
 
-# Audio debug tools
-PRODUCT_PACKAGES += \
+# Audio debug tools, no need in release builds.
+PRODUCT_PACKAGES_DEBUG += \
     tinycap2 \
     tinymix2 \
     tinypcminfo2 \
