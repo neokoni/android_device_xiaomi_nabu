44c81cfa84a140cf2c30fb4e10f6100e9e91d95a
Harshit Jain <reach@harsh1998.dev>
2026-07-07 05:38:39 +0530

    nabu: add back vibrator hal for external devices

--- device.mk
+++ device.mk
@@ -443,6 +443,13 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     vndservicemanager
 
+# Vibrator
+PRODUCT_PACKAGES += \
+    vendor.qti.hardware.vibrator.service
+
+PRODUCT_COPY_FILES += \
+    vendor/qcom/opensource/vibrator/excluded-input-devices.xml:$(TARGET_COPY_OUT_VENDOR)/etc/excluded-input-devices.xml
+
 # Wi-Fi
 PRODUCT_PACKAGES += \
     android.hardware.wifi-service \
